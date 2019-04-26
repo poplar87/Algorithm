ebc6aba78be73ffa3705be5a1717ed2f336ed6d0
poplar87 <46121498+poplar87@users.noreply.github.com>
2019-04-26 16:00:28 +0800

    34、第一个只出现一次的字符
    34、第一个只出现一次的字符  
    387. First Unique Character in a String

---
+++ classic algorithms/firstUniqChar.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] ss=new int[26];
+        for(int i=0;i<s.length();i++){
+            ss[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<s.length();i++){
+            if(ss[s.charAt(i)-'a']==1)return i;
+        }
+        return -1;
+    }
+}
