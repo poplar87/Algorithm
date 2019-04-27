137fcf1036810a6a7f53e7e4828c3820942bbf76
poplar87 <46121498+poplar87@users.noreply.github.com>
2019-04-27 14:51:05 +0800

    11、二进制中1的个数

--- classic algorithms/hammingWeight.java
+++ classic algorithms/hammingWeight.java
@@ -2,7 +2,7 @@ public int hammingWeight(int n) {
         int count=0;
         while(n!=0){
             ++count;
-            n=(n-1)&n;
+           n=(n-1)&n;
         }
         return count;
     }
