a5fd864588db1a8a571c71fc114ff3b6c99fe9cd
poplar87 <46121498+poplar87@users.noreply.github.com>
2019-04-27 15:10:49 +0800

    12 数值的整数次方
    50. Pow(x, n)

--- classic algorithms/pow.java
+++ classic algorithms/pow.java
@@ -1,9 +1,17 @@
-public double pow(double x, int n) {
-        if(n == 0)
-            return 1;
+
+      public double myPow(double x, int n) {
+       if(n==0) return 1;
+        boolean minN = false;
         if(n<0){
-            n = -n;
             x = 1/x;
+            if(n == Integer.MIN_VALUE){ //如果是最大负数
+                n = n+1; //把它变成正数能表示的范围
+                minN = true;
+            }
+            n = -n;
         }
-        return (n%2 == 0) ? pow(x*x, n/2) : x*pow(x*x, n/2);
+        double ans = (n%2==0)?myPow(x*x, n/2):x*myPow(x*x, n/2);
+        if(!minN) return ans;
+        else return ans * x;
     }
+    
