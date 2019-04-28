5614ee51dc60d8d03d46d0a7861b120f2c5ff35e
poplar87 <46121498+poplar87@users.noreply.github.com>
2019-04-28 14:15:59 +0800

    16、合并两个排序的链表
    21. Merge Two Sorted Lists

--- classic algorithms/mergeTwoLists.java
+++ classic algorithms/mergeTwoLists.java
@@ -1,3 +1,7 @@
+/**
+
+21. Merge Two Sorted Lists
+*/
 public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if(l1==null)return l2;
         if(l2==null)return l1;
@@ -11,4 +15,21 @@ public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
             l2.next=mergeTwoLists(l1,ll2);
             return l2;
         }
+    }
+/**
+
+88. Merge Sorted Array
+*/
+public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i=m-1,j=n-1,k=m+n-1;
+        while(i>=0&&j>=0){
+            if(nums1[i]<=nums2[j])nums1[k--]=nums2[j--];
+            else nums1[k--]=nums1[i--];
+        }
+         // while(i>=0){
+         //    nums1[k--]=nums1[i--]; 
+         // }
+        while(j>=0){
+            nums1[k--]=nums2[j--]; 
+         }
     }
