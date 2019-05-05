0309ffd3d7972e4cce9229f0e4f7273cbfa002ee
poplar87 <46121498+poplar87@users.noreply.github.com>
2019-05-05 16:37:30 +0800

    57、对称的二叉树
    101. Symmetric Tree

---
+++ classic algorithms/isSymmetric.java
@@ -0,0 +1,8 @@
+ public boolean isSymmetric(TreeNode root) {
+        return isMirror( root, root);
+    }
+    public boolean isMirror(TreeNode l,TreeNode r) {
+        if(l==null&&r==null)return true;
+        if(l==null||r==null)return false;
+        return l.val==r.val&&isMirror(l.left,r.right)&&isMirror(l.right,r.left);
+    }
