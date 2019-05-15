579bd890f01f07c88cb4fa6bd0768db085645a90
poplar87 <46121498+poplar87@users.noreply.github.com>
2019-05-15 10:51:39 +0800

    13、调整数组顺序使奇数位于偶数前面
    328. Odd Even Linked List

---
+++ classic algorithms/oddEvenList.java
@@ -0,0 +1,18 @@
+/**
+ * 13、调整数组顺序使奇数位于偶数前面
+ * 328. Odd Even Linked List
+ /
+ public class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) return null;
+        ListNode odd = head, even = head.next, evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
