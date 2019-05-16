e589a4038c78a958be63ea942dfa672609a3ef2b
poplar87 <46121498+poplar87@users.noreply.github.com>
2019-05-16 17:33:23 +0800

    40、数组中只出现一次的数字

---
+++ classic algorithms/singleNumber.java
@@ -0,0 +1,8 @@
+/**136. Single Number
+public int singleNumber(int[] nums) {
+        int num=nums[0];
+        for(int i=1;i<nums.length;i++){
+           num^= nums[i];
+        }
+        return num;
+    }
