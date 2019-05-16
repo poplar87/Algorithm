60a5a124dd13f79d1e53120644a0dbab855aa33e
poplar87 <46121498+poplar87@users.noreply.github.com>
2019-05-16 17:12:52 +0800

    29、最小的K个数

--- classic algorithms/leastKthElement.java
+++ classic algorithms/leastKthElement.java
@@ -1,3 +1,28 @@
 
 215. Kth Largest Element in an Array
 230. Kth Smallest Element in a BST
+Solution one：
+quicksort
+Solution two：
+public int findKthLargest(int[] nums, int k) {
+    final PriorityQueue<Integer> pq = new PriorityQueue<>(nums.length,new Comparator<Integer>(){
+        @Override
+       public int compare(Integer i1,Integer i2){
+            return i2-i1;//maximum heap findLargestKElements   ; i1-i2 min heap  findLeastKElements
+        }
+        
+    });
+    for(int val : nums) {
+        pq.offer(val);
+
+        if(pq.size() > k) {
+            pq.poll();// remove the max element  for every poll action;
+        }
+    }// leave the least k elements
+        int l=pq.size();
+        for(int i=0;i<l;i++){
+            System.out.println(pq.poll());
+        }
+        return 0;
+   // return pq.peek();
+    }
