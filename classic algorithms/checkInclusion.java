d2e8b210ceaa39af771590f5d71357a7985275e5
poplar87 <46121498+poplar87@users.noreply.github.com>
2019-05-16 14:27:52 +0800

    27、字符串的排列

---
+++ classic algorithms/checkInclusion.java
@@ -0,0 +1,14 @@
+ /**
+ * 567. Permutation in String
+ public boolean checkInclusion(String s1, String s2) {
+      int [] map = new int [256];
+    for (char ch : s1.toCharArray()) map [ch] ++;
+    for (int idx = 0, start = 0; idx < s2.length(); idx ++) {
+        char ch = s2.charAt (idx);
+        if (-- map [ch] < 0) 
+            while (map [ch] != 0) 
+        map [s2.charAt (start ++)] ++;
+        else if (idx - start + 1 == s1.length()) return true;
+    }
+    return false;
+    }
