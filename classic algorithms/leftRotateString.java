841f81e69715111c4de93c4e926382d455e2cd0f
poplar87 <46121498+poplar87@users.noreply.github.com>
2019-05-17 17:09:37 +0800

    43、左旋转字符串

--- classic algorithms/leftRotateString.java
+++ classic algorithms/leftRotateString.java
@@ -1,4 +1,4 @@
-//
+//189. Rotate Array one is int[] ,the other is char[] ,totally the same
 public static char[] leftRotateString(char[] data, int n) {
     if (data == null || n < 0 || n > data.length) {
         return data;
