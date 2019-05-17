d048fdafb8453a717dd9e4dc6c1cba6bf5c17ba7
poplar87 <46121498+poplar87@users.noreply.github.com>
2019-05-17 14:48:04 +0800

    35 数组中的逆序对

--- classic algorithms/reversePairs.java
+++ classic algorithms/reversePairs.java
@@ -1,39 +1,55 @@
 //493. Reverse Pairs
-public int reversePairs(int[] nums) {
-        return mergesort_and_count(nums, 0, nums.length - 1);
+ public int reversePairs(int[] nums) {
+        if(nums==null||nums.length<=0) return 0;
+        return mergesort_and_count(nums,0, nums.length - 1);
     }
-    void merge(int[] nums, int start, int mid, int end)
-{
-    int n1 = (mid - start + 1);
-    int n2 = (end - mid);
-    int[] L=new int[n1]; int[] R=new int[n2];
-    for (int i = 0; i < n1; i++)
-        L[i] = nums[start + i];
-    for (int j = 0; j < n2; j++)
-        R[j] = nums[mid + 1 + j];
-    int i = 0, j = 0;
-    for (int k = start; k <= end; k++) {
-        if (j >= n2 || (i < n1 && L[i] <= R[j]))
-            nums[k] = L[i++];
-        else
-            nums[k] = R[j++];
-    }
-}
 
 int mergesort_and_count(int[] nums, int start, int end)
 {
-    if (start < end) {
-        int mid = (start + end) / 2;
-        int count = mergesort_and_count(nums, start, mid) + mergesort_and_count(nums, mid + 1, end);
-        int j = mid + 1;
-        for (int i = start; i <= mid; i++) {
-            while (j <= end && nums[i] > nums[j] * 2L)
-                j++;
-            count += j - (mid + 1);
-        }
-        merge(nums, start, mid, end);
-        return count;
+    if (start == end) 
+    {
+     return 0;
     }
-    else
-        return 0;
+        int mid = (end+start) / 2;
+        int count = mergesort_and_count(nums,start, mid)+mergesort_and_count(nums,mid + 1, end);
+      int i=mid,j=end;
+    while (i >= start&& j>=mid+1) {
+        
+            if ( nums[i] > nums[j] * 2L)
+            {  count += j - mid;
+                i--;}
+        else
+            j--;
+        }
+        
+    merge(nums,start,mid,end);
+        return count;
+    
+}
+void merge(int[] nums ,int start,int mid,int end){
+    int[] L=new int[mid-start+1];
+     int[] R=new int[end-mid];
+    
+    for(int i=0;i<mid-start+1;i++){
+        L[i]=nums[start+i];
+    }
+    for(int i=0;i<end-mid;i++){
+        R[i]=nums[i+mid+1];
+    }
+    int i=0,j=0,k=start;
+        while(i<mid-start+1 &&j<end-mid)
+        {  if(L[i]<R[j])
+        nums[k++]=L[i++];
+        else 
+            nums[k++]=R[j++];
+        }
+         while(i<mid-start+1)
+        { 
+        nums[k++]=L[i++];
+        }
+        while(j<end-mid)
+        { 
+        nums[k++]=R[j++];
+    }
+     
 }
