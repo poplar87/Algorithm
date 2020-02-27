166a43fb7a30ac28501b23215f5ed64fa73908a2
poplar87 <46121498+poplar87@users.noreply.github.com>
2020-02-27 13:43:08 +0800

    varifySequenceOfBST
    面试题33.  二叉搜索树的后序遍历序 列

--- classic algorithms/varifySequenceOfBST.java
+++ classic algorithms/varifySequenceOfBST.java
@@ -1,21 +1,21 @@
 //98. Validate Binary Search Tree
 boolean varifySequenceOfBST(int[] sequence){
+
 if(sequence==null||sequence.length<=0)return true;
 return varifySequenceOfBST(sequence,0,sequence.length-1);
 }
 boolean varifySequenceOfBST(int[] sequence,int start,int end){
 if(start>=end) return true;
 int root=sequence[end];
-for(int i=start;i<end;i++){
-if(sequence[i]>root)break;
+int i=start;
+while(i<end&&sequence[i]<=root){
+i++;
 }
+
 int j=i;
 // verify if right tree is BST
 for(;j<end;j++){
 if(sequence[j]<=root)return false;
 }
-int left=true;
-left=varifySequenceOfBST(sequence,start,i-1);
-int right=true;
-right=varifySequenceOfBST(sequence,i,end-1);
+return varifySequenceOfBST(sequence,start,i-1)&&varifySequenceOfBST(sequence,i,end-1);
 }
